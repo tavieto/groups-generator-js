6f67eceb318db3b450a2a1edeaaf029117df6f46
tavieto <tavieto@outlook.com>
2020-11-09 20:34:11 -0300

    fix bugs

--- app/scripts/buttons.js
+++ app/scripts/buttons.js
@@ -1,9 +1,16 @@
 function abreNovosGrupos() {
-    document.querySelector('#conteiner_up').style.visibility = 'visible';
-    document.querySelector('#conteiner_up').style.opacity = '1';
+    document.querySelector('#conteiner').style.visibility = 'visible';
+    document.querySelector('#conteiner').style.opacity = '1';
+    document.querySelector('#tela-transparente').style.width = "100vw"
+    document.querySelector('#tela-transparente').style.height = "100vh"
+    document.querySelector('#tela-transparente').style.backgroundColor = "rgba(0, 0, 0, 0.5)"
+    document.querySelector('#tela-transparente').setAttribute('onclick', 'fechaNovosGrupos()')
 }
 
-//function fechaNovosGrupos() {
-//    document.querySelector('#conteiner_up').style.visibility = 'hidden';
-//    document.querySelector('#conteiner_up').style.opacity = '0';
-//}
+function fechaNovosGrupos() {
+    document.querySelector('#conteiner').style.visibility = 'hidden';
+    document.querySelector('#conteiner').style.opacity = '0';
+    document.querySelector('#tela-transparente').style.width = "0"
+    document.querySelector('#tela-transparente').style.height = "0"
+    document.querySelector('#tela-transparente').style.backgroundColor = "rgba(0, 0, 0, 0)"
+}
