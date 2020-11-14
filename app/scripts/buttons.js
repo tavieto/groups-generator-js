186aa40fd8b8f8d55e798afba5378c5491c62e76
tavieto <tavieto@outlook.com>
2020-11-13 22:24:57 -0300

    Update

--- app/scripts/buttons.js
+++ app/scripts/buttons.js
@@ -1,6 +1,6 @@
 function abreNovosGrupos() {
-    document.querySelector('#conteiner').style.visibility = 'visible';
-    document.querySelector('#conteiner').style.opacity = '1';
+    document.querySelector('#criar-conjunto').style.visibility = 'visible';
+    document.querySelector('#criar-conjunto').style.opacity = '1';
     document.querySelector('#tela-transparente').style.width = "100vw"
     document.querySelector('#tela-transparente').style.height = "100vh"
     document.querySelector('#tela-transparente').style.backgroundColor = "rgba(0, 0, 0, 0.5)"
@@ -8,8 +8,25 @@ function abreNovosGrupos() {
 }
 
 function fechaNovosGrupos() {
-    document.querySelector('#conteiner').style.visibility = 'hidden';
-    document.querySelector('#conteiner').style.opacity = '0';
+    document.querySelector('#criar-conjunto').style.visibility = 'hidden';
+    document.querySelector('#criar-conjunto').style.opacity = '0';
+    document.querySelector('#tela-transparente').style.width = "0"
+    document.querySelector('#tela-transparente').style.height = "0"
+    document.querySelector('#tela-transparente').style.backgroundColor = "rgba(0, 0, 0, 0)"
+}
+
+function abreAddList() {
+    document.querySelector('#add-list').style.visibility = 'visible';
+    document.querySelector('#add-list').style.opacity = '1';
+    document.querySelector('#tela-transparente').style.width = "100vw"
+    document.querySelector('#tela-transparente').style.height = "100vh"
+    document.querySelector('#tela-transparente').style.backgroundColor = "rgba(0, 0, 0, 0.5)"
+    document.querySelector('#tela-transparente').setAttribute('onclick', 'fechaAddList()')
+}
+
+function fechaAddList() {
+    document.querySelector('#add-list').style.visibility = 'hidden';
+    document.querySelector('#add-list').style.opacity = '0';
     document.querySelector('#tela-transparente').style.width = "0"
     document.querySelector('#tela-transparente').style.height = "0"
     document.querySelector('#tela-transparente').style.backgroundColor = "rgba(0, 0, 0, 0)"
